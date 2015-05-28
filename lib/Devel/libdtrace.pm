dca21eca884d5265039d7125cda025d4ec0f4e95
Gordon Marler <gmarler@gmarler.com>
2015-05-28 13:04:38 -0400

    Eliminate License plugin in dist.ini, add basics for XS add

--- lib/Devel/libdtrace.pm
+++ lib/Devel/libdtrace.pm
@@ -1,6 +1,19 @@
-use strict;
-use warnings;
-
 package Devel::libdtrace;
 
+use strict;
+use warnings;
+use XSLoader;
+
+# VERSION
+
+# ABSTRACT: Perl XS interface to libdtrace
+
+#XSLoader::Load('Devel::libdtrace', $VERSION);
+
 1;
+
+__END__
+
+=head1 NAME
+
+Devel::libdtrace - Perl XS interface to libdtrace library
